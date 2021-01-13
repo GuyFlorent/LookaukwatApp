700885c9161977f0d1d07662c461379b339a77bf
Guy florent Wangueu wangueu <wangueujunior23@gmail.com>
2021-01-13 14:56:59 +0100

    pudh notification firebase ok for all users

---
+++ LookaukwatApp/LookaukwatApp.iOS/NotificationHelper.cs
@@ -0,0 +1,18 @@
+﻿using Foundation;
+using LookaukwatApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+
+namespace LookaukwatApp.iOS
+{
+    public class NotificationHelper : INotification
+    {
+        public void CreateNotification(string title, string message)
+        {
+            new NotificationDelegate().RegisterNotification(title, message);
+        }
+    }
+}

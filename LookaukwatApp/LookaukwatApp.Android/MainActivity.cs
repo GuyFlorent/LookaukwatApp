7fa66d77d828e82701c40a1a62f0fe8ed2911a9f
Guy florent Wangueu wangueu <wangueujunior23@gmail.com>
2021-01-29 15:24:30 +0100

    Add application link with lookaukwat.com everythings ok

--- LookaukwatApp/LookaukwatApp.Android/MainActivity.cs
+++ LookaukwatApp/LookaukwatApp.Android/MainActivity.cs
@@ -4,10 +4,38 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Android.Content;
 
 namespace LookaukwatApp.Droid
 {
     [Activity(Label = "Lookaukwat", Icon = "@mipmap/ico", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+
+    //Invite App Link
+    [IntentFilter(new[] { Android.Content.Intent.ActionView },
+                  DataScheme = "https",
+                  DataHost = "lookaukwat.com",
+                  DataPathPrefixes = new[] { "/Home", "/Job", "/ApartmentRental", "/Multimedia", "/Vehicule", "/Mode", "/House" },
+                  AutoVerify = true,
+                  Categories = new[] { Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable })]
+    [IntentFilter(new[] { Android.Content.Intent.ActionView },
+                  DataScheme = "http",
+                  DataHost = "lookaukwat.com",
+                  AutoVerify = true,
+                 DataPathPrefixes = new[] { "/Home", "/Job", "/ApartmentRental", "/Multimedia", "/Vehicule", "/Mode", "/House" },
+                  Categories = new[] { Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable })]
+    
+    [IntentFilter(new[] { Android.Content.Intent.ActionView },
+                  DataScheme = "https",
+                  DataHost = "lookaukwat.azurewebsites.net",
+                  DataPathPrefixes = new[] { "/Home", "/Job", "/ApartmentRental", "/Multimedia", "/Vehicule","/Mode", "/House" },
+                  AutoVerify = true,
+                  Categories = new[] { Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable })]
+    [IntentFilter(new[] { Android.Content.Intent.ActionView },
+                  DataScheme = "http",
+                  DataHost = "lookaukwat.azurewebsites.net",
+                  AutoVerify = true,
+                  DataPathPrefixes = new[] { "/Home", "/Job", "/ApartmentRental", "/Multimedia", "/Vehicule", "/Mode", "/House" },
+                  Categories = new[] { Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable })]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -23,6 +51,11 @@ namespace LookaukwatApp.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            base.OnNewIntent(intent);
         }
 
     }
--- LookaukwatApp/LookaukwatApp/App.xaml.cs
+++ LookaukwatApp/LookaukwatApp/App.xaml.cs
@@ -1,4 +1,10 @@
 ﻿using LookaukwatApp.Services;
+using LookaukwatApp.ViewModels.Appartment;
+using LookaukwatApp.ViewModels.House;
+using LookaukwatApp.ViewModels.Job;
+using LookaukwatApp.ViewModels.Mode;
+using LookaukwatApp.ViewModels.Multimedia;
+using LookaukwatApp.ViewModels.Vehicule;
 using LookaukwatApp.Views;
 using System;
 using Xamarin.Essentials;
@@ -20,6 +26,110 @@ namespace LookaukwatApp
             Resources.Add("FullScreenWidth", fullScreenWidth);
             MainPage = new AppShell();
         }
+
+
+        protected override void OnAppLinkRequestReceived(Uri uri)
+        {
+            if (uri.Host.EndsWith("lookaukwat.com", StringComparison.OrdinalIgnoreCase) ||
+                uri.Host.EndsWith("lookaukwat.azurewebsites.net", StringComparison.OrdinalIgnoreCase))
+            {
+
+                if (uri.Segments != null && uri.Segments.Length == 4)
+                {
+                    var Controler = uri.Segments[1].Replace("/", "");
+                    var msg = uri.Segments[2];
+                    var id = uri.Segments[3].Replace("/", "");
+
+                    switch (Controler)
+                    {
+                        case "Home":
+                           
+                                Device.BeginInvokeOnMainThread(async () =>
+                                {
+                                    await Shell.Current.GoToAsync($"//MainPage/ItemsPage");
+                                    
+                                });
+                          
+                            break;
+
+                        case "Job":
+                            if (!string.IsNullOrEmpty(id))
+                            {
+                                Device.BeginInvokeOnMainThread(async () =>
+                                {
+                                    await Shell.Current.GoToAsync($"//MainPage/JobDetailPage?{nameof(JobDetailsViewModel.ItemId)}={id}");
+
+                                });
+                            }
+
+                            break;
+
+                        case "ApartmentRental":
+                            if (!string.IsNullOrEmpty(id))
+                            {
+                                Device.BeginInvokeOnMainThread(async () =>
+                                {
+                                    await Shell.Current.GoToAsync($"//MainPage/ApartDetailPage?{nameof(ApartDetailViewModel.ItemId)}={id}");
+                                });
+                            }
+
+                            break;
+
+                        case "Multimedia":
+                            if (!string.IsNullOrEmpty(id))
+                            {
+                                Device.BeginInvokeOnMainThread(async () =>
+                                {
+                                    await Shell.Current.GoToAsync($"//MainPage/MultimediaDetailPage?{nameof(MultimediaDetailViewModel.ItemId)}={id}");
+                                });
+                            }
+
+                            break;
+
+                        case "Vehicule":
+                            if (!string.IsNullOrEmpty(id))
+                            {
+                                Device.BeginInvokeOnMainThread(async () =>
+                                {
+
+                                    await Shell.Current.GoToAsync($"//MainPage/VehiculeDetailPage?{nameof(VehiculeDetailViewModel.ItemId)}={id}");
+                                });
+                            }
+
+                            break;
+
+                        case "Mode":
+                            if (!string.IsNullOrEmpty(id))
+                            {
+                                Device.BeginInvokeOnMainThread(async () =>
+                                {
+                                    await Shell.Current.GoToAsync($"//MainPage/ModeDetailPage?{nameof(ModeDetailViewModel.ItemId)}={id}");
+                                });
+                            }
+
+                            break;
+
+                        case "House":
+                            if (!string.IsNullOrEmpty(id))
+                            {
+                                Device.BeginInvokeOnMainThread(async () =>
+                                {
+                                    
+                                    await Shell.Current.GoToAsync($"//MainPage/HouseDetailPage?{nameof(HouseDetailViewModel.ItemId)}={id}");
+                                });
+                            }
+
+                            break;
+
+                        default:
+                            Launcher.OpenAsync(uri);
+                            break;
+                    }
+                }
+            }
+        }
+
+
 
         protected override void OnStart()
         {
